c5b7ad4bc6b539f262b8f308259b08552267cd3b
Jacob Feng <jfeng4@ualberta.ca>
2023-05-23 12:13:01 -0600

    Simplified variables by removing redundant and unused vars

--- src/components/ExerciseRow.js
+++ src/components/ExerciseRow.js
@@ -1,21 +1,14 @@
 // todo: create this thing so that each row can have varying cell lengths.
 // we will use inline styles to do this
 
-const ExerciseRow = ({exerciseObj, numSets}) => {
-    const colWidth = (100 / numSets).toString() + '% '
-    const cssCols = colWidth.repeat(numSets)
-    const rowStyle = {
-        display: "grid",
-        gridTemplateColumns: cssCols
-    }
-    console.log(rowStyle)
+const ExerciseRow = ({exerciseObj}) => {
     return (
         <div className='exercise-row'>
             <div className='grid-item'>{exerciseObj.date}</div>
             <div className='grid-item'>{exerciseObj.name}</div>
             <div className='grid-item'>{exerciseObj.sets}</div>
             <div className='grid-item'>{exerciseObj.reps}</div>
-            {[...Array(numSets).keys()].map((key) => (<input key={key}></input>))}
+            {[...Array(parseInt(exerciseObj.sets)).keys()].map((key) => (<input key={key}></input>))}
         </div>
     )
 }
