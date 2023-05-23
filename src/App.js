1eca464370d91b59a39d1da9dbfc99f0e0d868a5
Jacob Feng <jfeng4@ualberta.ca>
2023-05-22 23:23:28 -0600

    Removed template code

--- src/App.js
+++ src/App.js
@@ -2,38 +2,7 @@ import { useState } from 'react'
 import './index.css'
 import ExerciseForm from './components/ExerciseForm'
 
-const TopAnecdote = ({anecdote, numVotes}) => {
-  return (
-    <div>
-      <h2>Anecdote with the most votes</h2>
-      {anecdote}
-      <Votes numVotes={numVotes}/>
-    </div>
-  )
-}
-
-const Votes = ({numVotes}) => {
-  return (
-    <div>
-      has {numVotes} votes
-    </div>
-  )
-}
-
 const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients'
-  ]
-   
-  const [selected, setSelected] = useState(0)
-  const [points, setVotes] = useState(new Uint8Array(anecdotes.length))
-
   const [exercises, setExercises] = useState([])
 
   const [currExercise, setCurrExercise] = useState('')
@@ -41,35 +10,9 @@ const App = () => {
   const [currRep, setCurrRep] = useState('')
   const [date, setDate] = useState(new Date().toLocaleDateString())
 
-
-  const handleRandomQuote = () => {
-    const randRange = Math.floor(Math.random() * anecdotes.length)
-    console.log(randRange)
-    return setSelected(randRange)
-  }
-
-  const handleVote = () => {
-     const updatedPoints = [...points]
-
-     console.log("voted")
-     updatedPoints[selected] += 1
-
-     console.log("updated Points:", updatedPoints)
-
-     return setVotes(updatedPoints)
-  }
-
   return (
     <div>
       <h1>Oly tracker v1</h1>
-      <div>
-        <div>
-          {anecdotes[selected]}
-        </div>
-        <Votes numVotes={points[selected]}/>
-        <button onClick={handleVote}>vote</button>
-        <button onClick={handleRandomQuote}>next anecdote</button>
-      </div>
       <ExerciseForm
         exercises={exercises}
         currExercise={currExercise}
@@ -82,7 +25,6 @@ const App = () => {
         setCurrRep={setCurrRep}
         setCurrDate={setDate}
       />
-      <TopAnecdote numVotes={Math.max(...points)} anecdote={anecdotes[points.indexOf(Math.max(...points))]}/>
     </div>
   )
 }
