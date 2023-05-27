e45daa992ca7056f1a82632f007ab81dab1fa53d
Jacob Feng <jfeng4@ualberta.ca>
2023-05-27 14:48:39 -0600

    Added cors for different origin requests

--- oly-backend/app.js
+++ oly-backend/app.js
@@ -4,6 +4,7 @@ const app = express()
 const exerciseRouter = require('./controllers/exercise')
 const middleware = require('./utils/middleware')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const logger = require('./utils/logger')
 
 mongoose.set("strictQuery", false)
@@ -21,6 +22,7 @@ mongoose.connect(config.MONGODB_URI)
 app.use(express.json())
 app.use(middleware.requestLogger)
 
+app.use(cors)
 app.use("/api/exercises", exerciseRouter)
 app.use(middleware.errorHandler)
 
