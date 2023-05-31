171e232cc04d6a446d546a0e13228abd4e2abb43
Jacob Feng <jfeng4@ualberta.ca>
2023-05-30 18:19:20 -0600

    Removed cluttering comments

--- oly-backend/models/exercise.js
+++ oly-backend/models/exercise.js
@@ -11,10 +11,8 @@ const exerciseSchema = new mongoose.Schema({
 exerciseSchema.set("toJSON", {
     transform: (document, returnedObject) => {
         returnedObject.id = returnedObject._id.toString()
-        // const date = returnedObject.date.toString()
-        // returnedObject.date = date
+
         const convertedDate = new Date(returnedObject.date)
-        console.log(convertedDate)
         returnedObject.date = convertedDate.toLocaleDateString('en-US', {day: "2-digit", month:"2-digit", year: "2-digit"})
 
         delete returnedObject._id
