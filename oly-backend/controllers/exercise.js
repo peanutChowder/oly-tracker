41d029593f4c64031133f9799ced8fe5f7386912
Jacob Feng <jfeng4@ualberta.ca>
2023-05-30 21:42:13 -0600

    Added put request for updating single exercise

--- oly-backend/controllers/exercise.js
+++ oly-backend/controllers/exercise.js
@@ -1,5 +1,6 @@
 const exerciseRouter = require('express').Router()
 const Exercise = require('../models/exercise')
+const logger = require('../utils/logger')
 
 exerciseRouter.get("/", async (request, response, next) => {
     const exercises = await Exercise.find({})
@@ -17,6 +18,21 @@ exerciseRouter.get("/:id", (request, response, next) => {
             }
         })
         .catch(error => next(error))
+})
+
+exerciseRouter.put("/:id", async (request, response, next) => {
+    const updatedExercise = await Exercise.findByIdAndUpdate(
+        request.params.id, 
+        request.body, 
+        {new: true, runValidators: true}
+    )
+
+    if (updatedExercise === null) {
+        logger.error(`PUT request failed on id '${request.params.id}'`)
+        return response.status(404).json({error: `Exercise does not exist: '${request.body}'`})
+    }
+
+    response.status(204).json(request.body)
 })
 
 exerciseRouter.post("/", async (request, response, next) => {
