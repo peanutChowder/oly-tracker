72e53bcb887b32f37fb3ada967a9135187c3a66d
Jacob Feng <jfeng4@ualberta.ca>
2023-05-30 21:45:21 -0600

    Added cors back in

--- oly-backend/app.js
+++ oly-backend/app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const exerciseRouter = require('./controllers/exercise')
 const middleware = require('./utils/middleware')
+const cors = require("cors")
 const mongoose = require('mongoose')
 
 const logger = require('./utils/logger')
@@ -19,6 +20,7 @@ mongoose.connect(config.MONGODB_URI)
     })
 
 
+app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
 
